1d5760ed121a80ad9624817555f1117815b1e723
Phil Fernandez <norcal618@gmail.com>
2021-02-04 19:06:46 -0800

    .

---
+++ src/main/java/com/googhcl/hellojwt/MyJwtProjApplication.java
@@ -0,0 +1,13 @@
+package com.googhcl.hellojwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJwtProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJwtProjApplication.class, args);
+	}
+
+}
