b26ab82c36db9db26840bf746dd9e2fea98a732b
Phil Fernandez <norcal618@gmail.com>
2021-02-06 00:36:38 -0800

    crossorigin all the things

--- src/main/java/com/googhcl/hellojwt/controller/ToDoController.java
+++ src/main/java/com/googhcl/hellojwt/controller/ToDoController.java
@@ -16,7 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:3000")
+// @CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin
 public class ToDoController {
 
     @Autowired
